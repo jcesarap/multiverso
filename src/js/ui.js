f877463038dd2791cec4f68d7705d188c604a3e9
César <jcesarap@protonmail.com>
2025-07-24 10:06:04 -0300

    Finished 1st screen - Adding second

---
+++ src/js/ui.js
@@ -0,0 +1,31 @@
+// Managing menus
+
+
+// SOURCING DOM ELEMENTS
+
+const profileButton = document.getElementById('profile'); // It stores a REFERENCE to that (DOM) ELEMENT in a VARIABLE - so you can attach behavior to it.
+const profileOverlay = document.getElementById('profile-overlay'); // Same as above here
+
+const closeButton = document.getElementById('close'); // Same as above here
+
+const dropdownButton = document.getElementById('dropdown-button'); // Same as above here
+const dropdownMenu = document.getElementById('dropdown-menu'); // Same as above here
+const dropdownIcon = document.getElementById('dropdown-icon');
+
+// INTERACTIONS
+
+profileButton.addEventListener('click', () => { // the method/function      ".addEventListener"     receives another function (anonymous) as its 2st parameter
+    profileOverlay.classList.toggle('show'); // It uses it when appropriate
+    // References DOM element defined in JS above; classList manipulates class property of DOM element; adds/removes (toggles) show
+});
+
+closeButton.addEventListener('click', () => { // the method/function      ".addEventListener"     receives another function (anonymous) as its 2st parameter
+    profileOverlay.classList.toggle('show'); // It uses it when appropriate
+    // References DOM element defined in JS above; classList manipulates class property of DOM element; adds/removes (toggles) show
+});
+
+dropdownButton.addEventListener('click', () => { // the method/function      ".addEventListener"     receives another function (anonymous) as its 2st parameter
+    dropdownMenu.classList.toggle('show'); // It uses it when appropriate
+    // References DOM element defined in JS above; classList manipulates class property of DOM element; adds/removes (toggles) show
+    dropdownIcon.textContent = dropdownMenu.classList.contains('show') ? '∧' : '∨';
+});
